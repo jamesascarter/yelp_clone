69784b34f16e2400848815eaa3c0beb32cfee6c9
jamesascarter <jamesascarter@gmail.com>
2014-11-03 18:25:58 +0000

    reviews functionality added

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -64,14 +64,16 @@ end
   end
 end
 
-# describe 'deleting restaurants' do
+describe 'deleting restaurants' do
 
-#   before do
-#     Restaurant.create(:name => "KFC")
-#   end
+  before do
+    Restaurant.create(:name => "KFC")
+  end
 
-#   it "removes a restaurant when a user clicks a delete link" do
-#     visit '/restaurants'
-#     click_link 'Delete KFC'
-#     expect(page)not_to have_content 'KFC'
-#   end
+  it "removes a restaurant when a user clicks a delete link" do
+    visit '/restaurants'
+    click_link 'Delete KFC'
+    expect(page).not_to have_content 'KFC'
+    expect(page).to have_content 'Restaurant deleted successfully'
+  end
+end
