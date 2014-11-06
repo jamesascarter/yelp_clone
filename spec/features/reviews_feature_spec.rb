e46ab4ccbe2b4322fc19a4681854200efce36b2b
jamesascarter <jamesascarter@gmail.com>
2014-11-06 12:28:24 +0000

    users can only delete their own reviews

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -5,37 +5,45 @@ describe 'reviewing' do
   before do
     @james = User.create(email: 'james@gmail.com', password:'11111111', password_confirmation:'11111111')
     @james.restaurants.create(name: 'KFC')
+
   end
 
   it 'allows users to leave a review using a form' do
-    visit '/'
-    click_link 'Review KFC'
-    fill_in "Thoughts", with: "so so"
-    select '3', from: 'Rating'
-    click_button 'Leave Review'
+    create_review
     expect(current_path).to eq '/restaurants'
     expect(page).to have_content('so so')
   end
 
   it 'only allows users to only create one review per restaurant' do
     login_as @james
-    visit '/'
-    click_link 'Review KFC'
-    fill_in "Thoughts", with: "so so"
-    select '3', from: 'Rating'
-    click_button 'Leave Review'
+    create_review
     expect(page).not_to have_content('Review KFC')
   end
 
   it 'allows users to delete reviews' do
     login_as @james
+    create_review
+    click_link 'Delete review'
+    expect(page).not_to have_content('so so')
+  end
+
+  it 'only allows users to delete their own reviews' do
+    login_as @elena
+    create_review
+    login_as @james
+    visit '/'
+    expect(page).not_to have_content('Delete review')
+
+  end
+
+
+
+end
+
+def create_review
     visit '/'
     click_link 'Review KFC'
     fill_in "Thoughts", with: "so so"
     select '3', from: 'Rating'
     click_button 'Leave Review'
-    click_link 'Delete review'
-    expect(page).not_to have_content('so so')
-  end
-
 end
